357150458b8c4aaace35128b9019435d3d63fef4
abdelrhmanlahmed@yahoo.com <abdelrhmanlahmed@yahoo.com>
2024-05-02 17:36:14 +0300

    add files

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Cardcomps } from '../cardcomps';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  products : Cardcomps [] = [
+    {title: "Camera",img:'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600',price:5689,desc:"a black sony camera",ibuy:'Add to card'},
+    {title: "Camera",img:'https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',price:5689,desc:"a black sony camera",ibuy:'Add to card'},
+    {title: "Home",img:'https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',price:354,desc:"sony camera",ibuy:'Add to card'},
+    {title: "Camera",img:'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600',price:5689,desc:"a black sony camera",ibuy:'Add to card'},
+
+  ]
+  textins = this.products[0].ibuy
+
+  changetext (title: string,price : number){
+    console.log(title,price);
+    
+    this.textins = 'done and done'
+  }
+
+}
---
+++ src/app/cardcomps.ts
@@ -0,0 +1,7 @@
+export interface Cardcomps {
+    title : string,
+    img: string,
+    price : number;
+    desc : string;
+    ibuy : string;
+}
